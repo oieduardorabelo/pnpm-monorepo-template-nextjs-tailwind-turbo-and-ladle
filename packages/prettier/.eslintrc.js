0a403c21c8b25c194966394f5e25b5cee2649760
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-08-19 08:52:23 +1200

    chore: share eslint

---
+++ packages/prettier/.eslintrc.js
@@ -0,0 +1,9 @@
+const eslintBaseConfig = require('@monorepo/eslint/eslint.base');
+
+module.exports = {
+  ...eslintBaseConfig,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+  },
+};
